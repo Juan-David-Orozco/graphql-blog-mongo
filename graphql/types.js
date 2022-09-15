79162a7e10b5fc3e438023fd49d6c2aceae881db
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-09-15 16:24:57 -0500

    define post author

--- graphql/types.js
+++ graphql/types.js
@@ -1,4 +1,5 @@
 const { GraphQLObjectType, GraphQLString, GraphQLID } = require("graphql");
+const { User } = require('../models')
 
 const UserType = new GraphQLObjectType({
   name: "UserType",
@@ -20,8 +21,10 @@ const PostType = new GraphQLObjectType({
     id: {type: GraphQLID},
     title: {type: GraphQLString},
     body: {type: GraphQLString},
-    authorId: {type: GraphQLID},
-  }
+    author: { type: UserType, resolve(parent) {
+      return User.findById(parent.authorId)
+    } },
+  },
 })
 
 module.exports = {
